d7d5bbeafb5cdbbd04bc79215d34c736772a1b25
pedrokaut <lexus04111@gmail.com>
2025-03-06 08:43:28 -0300

    mudanças

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
+struct grupo{
+    char nome[20];
+    int idade;
+    float media;
+};
 int main(){
-cout << "hello word"<< endl;
+    grupo alunos[3];
+    int i;
+    for(i=1;i<4;i++){
+    cout << "digite o nome do aluno: "<< i << endl;
+    cin >> alunos[i].nome;
+    cout << "digite a idade do aluno: "<< i << endl;
+    cin >> alunos[i].idade;
+    cout << "digite a media do aluno: "<< i << endl;
+    cin >> alunos[i].media;
+    cout << "nome: " << alunos[i].nome << endl;
+    cout << "idade: " << alunos[i].idade << endl;
+    cout << "media: " << alunos[i].media << endl;
+    }
+    
+
+
 return 0;
 }
